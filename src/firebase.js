55bea2d5645598323b1a23232067fa83f2b9c562
naivenave <91294407+naivenave@users.noreply.github.com>
2021-10-26 00:37:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDtnOBOV5OV0_793xG_gJj5RYhpt8pQxY4",
-    authDomain: "amzn-nam-cl.firebaseapp.com",
-    projectId: "amzn-nam-cl",
-    storageBucket: "amzn-nam-cl.appspot.com",
-    messagingSenderId: "763301145137",
-    appId: "1:763301145137:web:b7f714721a29534cb2c310"
+    apiKey: <Your api key after login into Firebase>,
+    authDomain: <Auth Domain>,
+    projectId: <Project Id>,
+    storageBucket: <Bucket Name>,
+    messagingSenderId: <Sender Id>,
+    appId: <App id>
   };
